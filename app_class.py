68a9199e7cd92b2896e90275b23d9e8330f69cad
Nguyen Hai-Son <nhson17@apcs.vn>
2020-04-30 21:51:25 +0700

    Finish add text to intro

--- app_class.py
+++ app_class.py
@@ -11,27 +11,48 @@ class App:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.state = 'intro'
+        self.state = 'start'
 
     def run(self):
         while self.running:
-            if self.state == 'intro':
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
+########################################### HELPER FUNCTIONS ###########################################
+    def draw_text(self, words, screen, position, size, color, font_name, center=False):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, color)
+        text_size = text.get_size()
+        if center:
+            position[0] = position[0] - text_size[0]//2
+            position[1] = position[1] - text_size[1]//2
+        screen.blit(text, position)
+
 ########################################### INTRO FUNCTIONS ###########################################
 
-    def intro_events(self):
+    def start_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'
 
-    def intro_update(self):
+    def start_update(self):
         pass
 
-    def intro_draw(self):
+    def start_draw(self):
+        self.screen.fill(BLACK)
+        self.draw_text('HIGH SCORE', self.screen, [4, 0], START_TEXT_SIZE,
+                       (255, 255, 255), START_FONT)
+        self.draw_text('PUSH SPACE TO START', self.screen, [WIDTH//2, HEIGHT//2 - 50], START_TEXT_SIZE,
+                       (170, 132, 58), START_FONT, center=True)
+        self.draw_text('1 PLAYER ONLY', self.screen, [WIDTH//2, HEIGHT//2 + 5], START_TEXT_SIZE,
+                       (33, 137, 156), START_FONT, center=True)
+        self.draw_text('SELECT LEVEL ', self.screen, [WIDTH//2 - 15, HEIGHT//2 + 55], START_TEXT_SIZE,
+                       (247, 243, 242), START_FONT, center=True)
         pygame.display.update()
---
+++ settings.py
@@ -0,0 +1,12 @@
+# screen settings
+WIDTH, HEIGHT = 448, 596
+FPS = 60
+
+# color settings
+BLACK = (0, 0, 0)
+
+# font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+# player settings
+# ememy settings
